42d8b74e1addf2df89a7bc7334af0ea2259a5cdc
Jan Mottl <mottja@delta-skola.cz>
2024-09-23 08:51:00 +0200

    kk

---
+++ Operatory/Operatory/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Operatory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+
+            //Sčítání
+            Console.WriteLine(y+x);
+            //Odčítání
+            Console.WriteLine(y - x);
+            //Násobení
+            Console.WriteLine(x * y);
+            //Dělení
+            Console.WriteLine(y / x);
+
+            //Modulo
+            Console.WriteLine(6 % 2);
+            Console.WriteLine(7 % 2);
+
+            //Inkriminace (zvýšení hodnoty čísla o 1)
+            x++;
+
+            //Deinkriminace (snížení hodnoty čísla o 1)
+            x--;
+
+            //Přířazovací operátoy
+            //Zvýšení hodnoty x o 3 ( x = x + 3)
+
+            x += 3; // x = x + 3;
+            x -= 3; // x = x - 3;
+
+            //Porovnávací operátory
+
+            Console.WriteLine(x > y);
+            Console.WriteLine(x < y);
+
+            Console.WriteLine(5 == 5); //rovná se jsou DVĚ znamení "="
+
+            Console.WriteLine(6 >= 6); //větší, nebo rovno
+
+        }
+    }
+}
---
+++ Proc_obsahCtverce/Proc_obsahCtverce/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Proc_obsahCtverce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sem přijde váš kód
+
+            Console.WriteLine("Zadej mi stranu A");
+            // int stranaA = int.Parse(Console.ReadLine()); //Parsování vstupu na int
+            int stranaA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Obsah čtverce je {stranaA*stranaA}");
+            Console.WriteLine("Obsah čtverce je {0}", stranaA * stranaA);
+            Console.WriteLine("Obsah čtverce je " + stranaA * stranaA);
+
+            // Konec prostoru pro kód
+        }
+    }
+}

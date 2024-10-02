6b3a5947a56599a6658fb576c0d6c5fa2d29793d
Jan Mottl <mottja@delta-skola.cz>
2024-10-02 10:03:56 +0200

    kk

---
+++ 02/Proc_SudLich/Proc_SudLich/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Proc_SudLich
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej mi číslo: ");
+            int cislo = int.Parse(Console.ReadLine());
+
+            if (cislo == 0)
+            {
+                Console.WriteLine("Číslo je nula!");
+            } else if (cislo % 2 == 0)
+            {
+                Console.WriteLine("Čislo je sudé!");
+            } else
+            {
+                Console.WriteLine("Číslo je liché!");
+            }
+        }
+    }
+}

3baabe0893784ec2ab2283195f29f13663b07ac5
Jan Mottl <mottja@delta-skola.cz>
2024-10-14 09:14:58 +0200

    kk

---
+++ 02/Proc_DelkaSlova/Proc_DelkaSlova/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Proc_DelkaSlova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej 1. slovo");
+            string slovo1 = Console.ReadLine();
+            Console.WriteLine("Zadej 2. slovo");
+            string slovo2 = Console.ReadLine();
+
+            if(slovo1.Length == slovo2.Length) //string.Lenght vrací délku daného textu v čísle
+            {
+                Console.WriteLine("Slova jsou stejně dlouhá");
+            } else if (slovo1.Length > slovo2.Length)
+            {
+                Console.WriteLine($"1. slovo: {slovo1} je delší než 2. slovo");
+            } else
+            {
+                Console.WriteLine($"2. slovo: {slovo2} je delší než 1. slovo");
+            }
+        }
+    }
+}

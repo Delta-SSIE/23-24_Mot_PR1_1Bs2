75978af7a4b41754c8380457a70512258e24b7f8
Jan Mottl <mottja@delta-skola.cz>
2024-10-21 10:29:32 +0200

    kk

---
+++ 03/Proc_Mala_nasobilka/Proc_Mala_nasobilka/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Proc_Mala_nasobilka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej mi číslo a já ti vypíšu malou násobilkou tohoto čísla");
+            int cislo = int.Parse(Console.ReadLine());
+            int pocitadlo = 1;
+            
+            while(pocitadlo <= 10)
+            {
+                Console.WriteLine($"{cislo}x{pocitadlo}={cislo*pocitadlo}");
+                pocitadlo++;
+            }
+        }
+    }
+}

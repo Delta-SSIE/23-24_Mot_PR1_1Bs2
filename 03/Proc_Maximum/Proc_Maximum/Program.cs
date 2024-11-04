9a6140d7e58f55873e4b4a29d3a92743376efeff
Jan Mottl <mottja@delta-skola.cz>
2024-11-04 08:16:43 +0100

    kk

---
+++ 03/Proc_Maximum/Proc_Maximum/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Proc_Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo = 0;
+            int max = 0;
+
+            do
+            {
+                Console.WriteLine("Zadej mi číslo: ");
+                while (!int.TryParse(Console.ReadLine(), out cislo))
+                {
+                    Console.WriteLine("Tohle není číslo!");
+                }
+                if (cislo > max)
+                {
+                    max = cislo;
+                }
+            } while (cislo > 0);
+
+            if (max == 0)
+            {
+                Console.WriteLine("Nebylo zadáno žádné číslo!");
+            } else
+            {
+                Console.WriteLine($"Nejvyšší zadané číslo bylo {max}");
+            }
+            
+        }
+    }
+}

c2602e70f704d8f2ba9802700c19b49111c1ea24
Jan Mottl <mottja@delta-skola.cz>
2024-11-13 09:11:17 +0100

    kk

---
+++ 04/Proc_Pikola/Proc_Pikola/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Proc_Pikola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 201; i+=10) //i = i +10
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Už jdu!");
+        }
+    }
+}

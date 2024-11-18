399428d1d9525e44ccd3f3eef9f46dc004061ca3
Jan Mottl <mottja@delta-skola.cz>
2024-11-18 08:32:53 +0100

    kk

---
+++ 04/Proc_Namaluj_ctverec/Proc_Namaluj_ctverec/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Proc_Namaluj_ctverec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int delka = 0;
+            Console.WriteLine("Jaká bude délka hrany čtverce?");
+            while(!int.TryParse(Console.ReadLine(),out delka))
+            {
+                Console.WriteLine("Zadej číslo!");
+            }
+
+            for (int i = 0; i < delka; i++)
+            {
+                for (int j = 0; j < delka; j++)
+                {
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04/Proc_PoSchodoch/Proc_PoSchodoch/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Proc_PoSchodoch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

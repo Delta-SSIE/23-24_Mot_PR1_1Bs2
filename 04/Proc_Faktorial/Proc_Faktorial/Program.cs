36ae6523002fd3b444bb09536b7ec0ecc7226e35
Jan Mottl <mottja@delta-skola.cz>
2024-11-18 08:30:31 +0100

    ok

---
+++ 04/Proc_Faktorial/Proc_Faktorial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Proc_Faktorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong cislo = 0;
+            Console.WriteLine("Napiš číslo a já vypíšu faktoriál tohoto čísla");
+            while (!ulong.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Zadal jsi špatný datový typ! Zadej číslo!");
+            }
+
+            for (ulong i = cislo-1; i > 0; i--)
+            {
+                cislo *= i;
+            }
+
+            Console.WriteLine(cislo);
+        }
+    }
+}

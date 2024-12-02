e0ff4b574f1802f1753c5d14712e7003b4460f62
Jan Mottl <mottja@delta-skola.cz>
2024-12-02 08:51:46 +0100

    kk

---
+++ 05/Proc_NajdiPrvek/Proc_NajdiPrvek/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Proc_NajdiPrvek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = new int[] { -11, 0, 5, -12, 13, 24, 19, 13, -17 };
+            int cislo = 0;
+            int index = -1; //proměnná pro index pro ověření nastavená nesmyslná hodnota
+
+            Console.WriteLine("Napiš mi číslo, které hledáš?");
+            while(!int.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Zadej čislo!");
+            }
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                if (cisla[i] == cislo)
+                {
+                    index = i;
+                    break; //vyskočení z cyklu ven
+                }
+            }
+
+            if(index == -1) //ověření nalezení čísla
+            {
+                Console.WriteLine($"{cislo} se nenachází v poli.");
+            } else
+            {
+                Console.WriteLine($"{cislo} se nachází v poli na indexu: {index}.");
+            }
+        }
+    }
+}

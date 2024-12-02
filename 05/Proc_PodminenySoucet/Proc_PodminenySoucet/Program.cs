a87f5243b64606727596d34b674abf389e608751
Jan Mottl <mottja@delta-skola.cz>
2024-12-02 08:33:09 +0100

    kk

---
+++ 05/Proc_PodminenySoucet/Proc_PodminenySoucet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Proc_PodminenySoucet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole = { 25, -56, -88, 5, 10, 65, 584, -8, -120, 186 };
+            int soucet = 0;
+
+            foreach (int cislo in pole)
+            {
+                if(cislo < 10 && cislo > -10)
+                {
+                    soucet += cislo;
+                }
+            }
+
+            Console.WriteLine(soucet);
+
+        }
+    }
+}

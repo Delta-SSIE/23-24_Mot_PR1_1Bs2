a30d68f40eb1298d247e1df44c4e925ee73e9b29
Jan Mottl <mottja@delta-skola.cz>
2024-12-02 08:43:47 +0100

    kk

---
+++ 05/Proc_DlouheRetezce/Proc_DlouheRetezce/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Proc_DlouheRetezce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] retezce = new string[5]; //Délka pole je 5, očekává 5 prvků
+
+            for (int i = 0; i < retezce.Length; i++) //procházení pole a načítání řetězců na jednotlivé pozice
+            {
+                Console.WriteLine($"Zadej {i+1}. řetězec");
+                retezce[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Ze zadaných řetězců jsou alespoň 6-znakové tyto:");
+            foreach (string slovo in retezce) //průchod pole retezce, již naplněného hodnotami
+            {
+                if(slovo.Length >= 6) //kontrola délky stringus
+                {
+                    Console.WriteLine(slovo);
+                }
+            }
+        }
+    }
+}

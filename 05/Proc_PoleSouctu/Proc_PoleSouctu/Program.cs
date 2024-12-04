452031c914c56e5bf94df4478d3ea814536218a6
Jan Mottl <mottja@delta-skola.cz>
2024-12-04 09:07:34 +0100

    kk

---
+++ 05/Proc_PoleSouctu/Proc_PoleSouctu/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Proc_PoleSouctu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { 20, 11, 0, 10, 7, 12, 83, 0, 0, 15 };
+            int[] pole_souctu = new int[cisla.Length];
+
+            for (int i = 0; i < pole_souctu.Length; i++)
+            {
+                /* Načítání hodnoty předchozího čísla v poli součtu
+                if(i != 0)
+                {
+                    pole_souctu[i] += pole_souctu[i - 1];
+                }
+                */
+                //Řešení přes průchod původním polem
+                for (int j = i; j >= 0; j--)
+                {
+                    pole_souctu[i] += cisla[j];
+                }
+            }
+
+            foreach (int cislo in pole_souctu) //výpis
+            {
+                Console.WriteLine(cislo);
+            }
+        }
+    }
+}

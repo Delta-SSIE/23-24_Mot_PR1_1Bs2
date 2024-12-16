38cd73f09b34848a72a86cc66053509648ff283c
Jan Mottl <mottja@delta-skola.cz>
2024-12-16 08:54:10 +0100

    kk

---
+++ 06/Proc_Kamaradi/Proc_Kamaradi/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Proc_Kamaradi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zavolání metody Kamaradi třikrát pokaždé s unikatními argumenty, které naplní dva parametry metody!
+            Kamaradi("Honza", "Pepa");
+            Kamaradi("Anička", "Rudolf");
+            Kamaradi("Adolf", "Herman");
+        }
+
+        //Metoda Kamaradi s dvěma string parametry
+        static void Kamaradi(string kamarad1, string kamarad2)
+        {
+            //Výpis do konzole s parametry
+            Console.WriteLine($"Moji kamarádi jsou {kamarad1} a {kamarad2}");
+        }
+    }
+}

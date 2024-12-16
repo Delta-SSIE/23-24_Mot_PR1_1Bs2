359a331b47090b9582776282d29e85a4ba7f3173
Jan Mottl <mottja@delta-skola.cz>
2024-12-16 09:08:41 +0100

    kk

---
+++ 06/Proc_DelsiSlovo/Proc_DelsiSlovo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Proc_DelsiSlovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej první slovo: ");
+            string slovo1 = Console.ReadLine();
+
+            Console.Write("Zadej druhé slovo: ");
+            string slovo2 = Console.ReadLine();
+
+            string delsiSlovo = DelsiSlovo(slovo1, slovo2); //Zavolání metody s dvěma argumenty, vracící hodnotu do proměnné delsiSlovo
+            Console.WriteLine($"Když porovnáme {slovo1} a {slovo2}, pak delší je {delsiSlovo}.");
+        }
+
+        static string DelsiSlovo(string a, string b) //dva parametry
+        {
+            if(a.Length > b.Length)
+            {
+                return a; //návratová hodnota
+            } else
+            {
+                return b; //návratová hodnota
+            }
+        }
+    }
+}

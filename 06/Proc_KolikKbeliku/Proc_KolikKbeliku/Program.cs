e02d6637be77a2e350af836a4f2b1078334e1ac4
Jan Mottl <mottja@delta-skola.cz>
2025-01-08 09:36:06 +0100

    kk

---
+++ 06/Proc_KolikKbeliku/Proc_KolikKbeliku/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Proc_KolikKbeliku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(KolikKbeliku(3,4,2.8,18));
+        }
+
+
+        static int KolikKbeliku(double delka, double vyska, double sirka, double vydatnost)
+        {
+            double plocha = 2 * (delka * vyska) + 2 * (sirka * vyska) + delka * sirka;
+            //4 stěny + 1 strop
+            Console.WriteLine(plocha);
+            double barva = plocha / vydatnost;
+            if(barva % 1 != 0) //pokud je číslo desetinné, tak přidá jeden kyblik
+            {
+                return (int) barva + 1;
+            }
+              return (int)barva; //vracení hodnoty v případě celého čísla
+        }
+    }
+}

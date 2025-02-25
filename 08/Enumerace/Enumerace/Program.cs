9bcf96be03117a3f913c83ffb1d31b64f762146f
Jan Mottl <mottja@delta-skola.cz>
2025-02-25 11:41:35 +0100

    kk

---
+++ 08/Enumerace/Enumerace/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Enumerace
+{
+    internal class Program
+    {
+        enum Pole
+        {
+            Voda,
+            Lod,
+            Zasah,
+            Srela
+        }
+
+        enum Level
+        {
+            Low,
+            Medium,
+            Hard
+        }
+        static void Main(string[] args)
+        {
+            Pole policko = Pole.Lod; //nastavení proměnné na enumerační hodnotu
+            Console.WriteLine(policko);
+
+            Console.WriteLine((int) policko); //převod enum hodnoty na číslo
+            Pole nove_policko = (Pole) ((int)policko)+1; //nová enum hodnota na základě číselné hodnoty jiné enum +1
+            Console.WriteLine(nove_policko);
+
+            policko = Pole.Voda;
+            Console.WriteLine(policko);
+
+            Level aktualni_uroven = Level.Low;
+            Console.WriteLine(aktualni_uroven);
+            aktualni_uroven = (Level)(int)aktualni_uroven+1;
+            Console.WriteLine(aktualni_uroven);
+
+        }
+    }
+}

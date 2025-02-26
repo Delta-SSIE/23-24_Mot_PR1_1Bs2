b482a7cf8e19a26b0eba9743fcd167ea49e913b2
Jan Mottl <mottja@delta-skola.cz>
2025-02-26 09:48:02 +0100

    kk

---
+++ 08/Uvod_OOP_sam/Uvod_OOP_sam/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uvod_OOP_sam
+{
+    enum Sex
+    {
+        Man,
+        Woman,
+        Other
+    }
+    internal class Human
+    {
+        public string name;
+        public Sex sex;
+        public int age;
+        public bool life = true;
+
+        //Konstruktor (metoda, která se zavolá při každém vytvoření objektu ze třídy Human)
+        public Human(string n, Sex s, int a)
+        {
+            //Načte hodnoty z parametrů do konkrétních vlastností u objektu
+            this.name = n;
+            this.sex = s;
+            this.age = a;
+        }
+
+
+        public string Introduction()
+        {
+            return $"Hello, I'm {name} and I'm {age} years old!";
+        }
+
+        public void Kill()
+        {
+            life = false;
+        }
+
+
+    }
+}
---
+++ 08/Uvod_OOP_sam/Uvod_OOP_sam/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Uvod_OOP_sam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Vytvoření objektu a přiřazení hodnot k jeho vlastnostem
+            Human person1 = new Human();
+
+            person1.name = "Josh";
+            person1.age = 50;
+            person1.sex = Sex.Man;
+            */
+
+            //Vytvoření objektu a přiřazení hodnot k vlastnostem pomocí KONSTRUKTORU
+            Human person1 = new Human("Josh", Sex.Man, 50);
+            Human person2 = new Human("Helga", Sex.Woman, 30);
+
+            Console.WriteLine(person1.Introduction());
+            Console.WriteLine(person1.life);
+
+            person1.Kill();
+            Console.WriteLine(person1.life);
+
+            Console.WriteLine(person2.Introduction());
+
+        }
+    }
+}

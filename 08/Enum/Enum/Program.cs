f7463a6bea2b334123ffc942ce5af12f4f970a77
Jan Mottl <mottja@delta-skola.cz>
2025-02-26 09:07:11 +0100

    kk

---
+++ 08/Enum/Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+        enum Obdobi
+        {
+            Jaro,
+            Leto,
+            Podzim,
+            Zima
+        }
+        static void Main(string[] args)
+        {
+            //Uložení enumerační hodnoty do proměnné
+            Obdobi aktualniObdobi = Obdobi.Zima;
+            Console.WriteLine(aktualniObdobi);
+
+            //ukázka indexu prvku v enumeraci
+            Console.WriteLine((int) aktualniObdobi); //převod aktualniObdobi na čislo
+
+            //posun enumerační hodnoty v rámci seznamu
+            aktualniObdobi--; //za Zimou není další období, proto nelze ++, ale lze -- (skok na Podzim)
+            Console.WriteLine(aktualniObdobi);
+
+
+
+        }
+    }
+}
---
+++ 08/Exepctions/Exepctions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exepctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vyjimka
+
+            try
+            {
+                int cislo = int.Parse(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Nebylo zadáno číslo");
+            }
+
+            try
+            {
+                int cislo2 = Convert.ToInt32("deset");
+            } catch (Exception error) //zachytává chybovou hlášku a ukládá do proměnné error
+            {
+                Console.WriteLine($"Chyba, zde je chybová hláška: {error}");
+            }
+        }
+    }
+}

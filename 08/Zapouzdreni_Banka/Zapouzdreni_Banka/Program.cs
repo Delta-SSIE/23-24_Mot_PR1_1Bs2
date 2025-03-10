f6b69eb25ff146776adf4a681476fa3589139b9a
Jan Mottl <mottja@delta-skola.cz>
2025-03-10 08:44:54 +0100

    kk

---
+++ 08/Zapouzdreni_Banka/Zapouzdreni_Banka/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Zapouzdreni_Banka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucet mujUcet = new Ucet();
+            mujUcet.JmenoDrzitele = "Lubor Zavora";
+            mujUcet.Bilance = 200;
+
+
+            while(true)
+            {
+                Console.WriteLine($"Učet na jméno: {mujUcet.JmenoDrzitele} \nZůstatek: {mujUcet.Bilance}");
+                Console.WriteLine("Pokud chceš vybrat, tak napiš vyber, pokud chceš vložit, napiš vlozit!");
+                string volba = Console.ReadLine();
+                switch(volba)
+                {
+                    case "vyber":
+                        double vyber = double.Parse(Console.ReadLine());
+                        mujUcet.Bilance -= vyber;
+                        break;
+                    case "vlozit":
+                        double vlozit = double.Parse(Console.ReadLine());
+                        mujUcet.Bilance += vlozit;
+                        break;
+                    default:
+                        Console.WriteLine("Neplatná možnost!");
+                        break;
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ 08/Zapouzdreni_Banka/Zapouzdreni_Banka/Ucet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zapouzdreni_Banka
+{
+    internal class Ucet
+    {
+        //private je opak public
+        //private lze manipulovat jen uvnitř Třídy
+        //private string jmeno_Drzitele;
+        //private položka
+        private double bilance;
+
+        //veřejná vlastnost
+        public double Bilance
+        {
+            //co se má stát, když uživatel zavolá danou vlastnost u objektu
+            get
+            {
+                //vracíme hodnotu private položky bilance
+                return bilance;
+            }
+            //Co se má stát, když uživatel manipuluje s hodnotou
+            set
+            {
+                double puv_hod = bilance;
+                //value je hodnota, když uživatel manipuluje s vlastností Bilance
+                bilance = value;
+                //testuje se,jestli po úpravě hodnoty uživatelem zadané se nerovná položka mínusové číslo, pokud ano, tak se vrací její původní hodnota
+                if(bilance < 0)
+                {
+                    bilance = puv_hod;
+                    Console.WriteLine("Nelze vybrat více než je na účtu!");
+                }
+            }
+        }
+
+        //get a set auto --> Veřejná vlatnost, která má jednoduchý getter a setter na načtení a změnu private položky (ta zde není definovaná)
+        public string JmenoDrzitele { get; set; }
+
+
+
+
+        //Metoda na odchycení chybného zadaní pro změnu vlastnosti Bilance
+        /*
+        public void SetBilance(double vyber)
+        {
+            if(vyber<Bilance)
+            {
+                Bilance -= vyber;
+            } else
+            {
+                Console.WriteLine("Nemůžeš vybrat tolik peněz. Na účtu jich tolik není!");
+            }
+        }
+        */
+    }
+}

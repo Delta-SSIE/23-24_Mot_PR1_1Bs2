77c79dc108775c5aa53dd7f90549c16585e9c472
Jan Mottl <mottja@delta-skola.cz>
2025-03-31 09:30:16 +0200

    kk

---
+++ 09/Zasobnik_mat_vyraz/Zasobnik_mat_vyraz/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Zasobnik_mat_vyraz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidovatVyraz("4 + 5"));
+            Console.WriteLine(ValidovatVyraz("(4 + 5) * (15 * 5"));
+            Console.WriteLine(ValidovatVyraz("(4 + 5) * (15 * 5)"));
+        }
+
+        static bool ValidovatVyraz(string vyraz)
+        {
+            Stack<int> zavorky = new Stack<int>();
+
+            bool podminka = true;
+            for (int i = 0; i < vyraz.Length; i++)
+            {
+                if ((vyraz[i] > 39 && vyraz[i] < 58) || vyraz[i] == 32)
+                {
+                    if (vyraz[i] == '(')
+                    {
+                        if(zavorky.Count > 0)
+                        {
+                            podminka = false;
+                            break;
+                        } else
+                        {
+                            zavorky.Push(vyraz[i]);
+                        }
+                    }
+                    if (vyraz[i] == ')')
+                    {
+                        if(zavorky.Count > 0)
+                        { 
+                            if(zavorky.Peek() == '(')
+                            {
+                                zavorky.Clear();
+                            } else
+                            {
+                                podminka = false;
+                                break;
+                            }
+                        } else
+                        {
+                            podminka = false;
+                            break;
+                        }
+                    }
+                } else
+                {
+                    podminka = false;
+                    break;
+                }
+            }
+
+
+            if(zavorky.Count > 0)
+            {
+                podminka = false;
+            }
+            return podminka;
+        }
+    }
+}

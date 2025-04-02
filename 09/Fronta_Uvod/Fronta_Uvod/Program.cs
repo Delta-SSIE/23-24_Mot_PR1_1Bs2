5fcce9aa9a71a589491bdde1e5b0ded009221326
Jan Mottl <mottja@delta-skola.cz>
2025-04-02 09:05:36 +0200

    kk

---
+++ 09/Fronta_Uvod/Fronta_Uvod/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Fronta_Uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarace fronta
+            Queue<string> fronta = new Queue<string>();
+
+            //přidání prvků do fronty
+            fronta.Enqueue("Jaromír");
+            fronta.Enqueue("Lumír");
+            fronta.Enqueue("Hanuš");
+            fronta.Enqueue("Jindřiška");
+
+            //Průchod frontou (odpředu)
+            foreach (string jmeno in fronta)
+            {
+                Console.WriteLine(jmeno);
+            }
+            Console.WriteLine("---------------");
+
+            //vyhazování prvků z fronty - z frontu
+            fronta.Dequeue(); //vyhodí Jaromíra
+            fronta.Dequeue(); //vyhodí Lumíra
+            //Průchod frontou (odpředu)
+            foreach (string jmeno in fronta)
+            {
+                Console.WriteLine(jmeno);
+            }
+            Console.WriteLine("---------------");
+
+            //vyhození a zařazení prvku do fronty (přesune prvek z frontu na back)
+            fronta.Enqueue(fronta.Dequeue());
+            foreach (string jmeno in fronta)
+            {
+                Console.WriteLine(jmeno);
+            }
+            Console.WriteLine("---------------");
+
+            //Peek
+            Console.WriteLine(fronta.Peek()); //vrátí prvek na frontu fronty
+
+            //Contain
+            Console.WriteLine(fronta.Contains("Hanuš"));
+        }
+    }
+}

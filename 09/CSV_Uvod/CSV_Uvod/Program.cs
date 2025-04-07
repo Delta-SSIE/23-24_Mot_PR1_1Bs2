24b09c5a69817e484ecd0b120aff145f77910eb8
Jan Mottl <mottja@delta-skola.cz>
2025-04-07 08:47:24 +0200

    kk

---
+++ 09/CSV_Uvod/CSV_Uvod/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace CSV_Uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] lide = new string[1000, 3];
+            StreamReader sr = new StreamReader("data.csv");
+            string radek = sr.ReadLine();
+            for (int i = 0; i < lide.GetLength(0); i++)
+            {
+                string[] data = radek.Split(",");
+                for (int j = 0; j < lide.GetLength(1); j++)
+                {
+                    lide[i, j] = data[j];
+                }
+                radek = sr.ReadLine();
+            }
+            sr.Close();
+
+            for (int i = 0; i < lide.GetLength(0); i++)
+            {
+                for (int j = 0; j < lide.GetLength(1); j++)
+                {
+                    Console.Write(lide[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            lide[0, 0] = "5";
+            lide[0, 1] = "John";
+            lide[0, 2] = "Gordon";
+
+            StreamWriter sw = new StreamWriter("data.csv");
+            for (int i = 0; i < lide.GetLength(0); i++)
+            {
+                radek = "";
+                for (int j = 0; j < lide.GetLength(1); j++)
+                {
+                    radek += lide[i, j] + ",";
+                }
+                sw.WriteLine(radek);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ 09/Soubor_Cteni_Zapis/Soubor_Cteni_Zapis/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Serialization;
+
+namespace Soubor_Cteni_Zapis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("text.txt"))
+            {
+                File.Create("text.txt");
+            }
+
+            while(true)
+            {
+                Console.WriteLine("Psani 1. Čtení 2.");
+                int volba = int.Parse(Console.ReadLine());  
+                switch (volba)
+                {
+                    case 1:
+                        StreamWriter sw = new StreamWriter("text.txt");
+                        Console.WriteLine("Piš, pokud budeš chtít přestat psát zadej prázdnou hodnotu");
+                        string text = Console.ReadLine();
+                        while(text != "")
+                        {
+                            sw.WriteLine(text);
+                            text = Console.ReadLine();
+                        }
+                        sw.Close();
+            
+                        break;
+                    case 2:
+                        StreamReader sr = new StreamReader("text.txt");
+                        string radek = sr.ReadLine();
+                        while(radek != null)
+                        {
+                            Console.WriteLine(radek);
+                            radek = sr.ReadLine();
+                        }
+                        sr.Close();
+                        break;
+                }
+            }
+
+
+        }
+
+    }
+}
